466efe3536f292418b8ba4a5d4fd1123c4f99f1e
Lepto <jasper@keespeek.com>
2021-09-08 13:28:05 -0700

    Added new tables to ERD

--- JasperC/Olympic_Project2_ERD_Schema.sql
+++ JasperC/Olympic_Project2_ERD_Schema.sql
@@ -2,45 +2,84 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "athlete" (
+CREATE TABLE "athletes" (
+    "athlete_id" int   NOT NULL,
     "last_name" varchar(255)   NOT NULL,
     "first_name" varchar(255)   NOT NULL,
-    "noc" varchar(255)   NOT NULL,
-    "discipline" varchar(255)   NOT NULL,
-    CONSTRAINT "pk_athlete" PRIMARY KEY (
-        "noc"
-     )
+    "country_id" int   NOT NULL,
+    "discipline_id" int   NOT NULL
 );
 
 CREATE TABLE "coaches" (
+    "coach_id" int   NOT NULL,
     "last_name" varchar(255)   NOT NULL,
     "first_name" varchar(255)   NOT NULL,
-    "noc" varchar(255)   NOT NULL,
-    "discipline" varchar(255)   NOT NULL,
-    "event" varchar(255)   NOT NULL
+    "country_id" int   NOT NULL,
+    "discipline_id" int   NOT NULL,
+    "event_id" int   NOT NULL
+);
+
+CREATE TABLE "countries" (
+    "country_id" int   NOT NULL,
+    "country_name" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_countries" PRIMARY KEY (
+        "country_id"
+     )
+);
+
+CREATE TABLE "events" (
+    "event_id" int   NOT NULL,
+    "event_name" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_events" PRIMARY KEY (
+        "event_id"
+     )
+);
+
+CREATE TABLE "disciplines" (
+    "discipline_id" int   NOT NULL,
+    "discipline_name" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_disciplines" PRIMARY KEY (
+        "discipline_id"
+     )
 );
 
 CREATE TABLE "medals" (
-    "noc" varchar(255)   NOT NULL,
+    "country_id" int   NOT NULL,
     "gold" int   NOT NULL,
     "total" int   NOT NULL,
     "rank" int   NOT NULL,
-    "rank_by_title" int   NOT NULL
+    "rank_by_total" int   NOT NULL,
+    CONSTRAINT "pk_medals" PRIMARY KEY (
+        "country_id"
+     )
 );
 
 CREATE TABLE "teams" (
+    "team_id" int   NOT NULL,
     "name" varchar(255)   NOT NULL,
-    "discipline" varchar(255)   NOT NULL,
-    "noc" varchar(255)   NOT NULL,
-    "event" varchar(255)   NOT NULL
+    "discipline_id" int   NOT NULL,
+    "country_id" int   NOT NULL,
+    "event_id" int   NOT NULL,
+    CONSTRAINT "pk_teams" PRIMARY KEY (
+        "team_id"
+     )
 );
 
-ALTER TABLE "coaches" ADD CONSTRAINT "fk_coaches_noc" FOREIGN KEY("noc")
-REFERENCES "athlete" ("noc");
+ALTER TABLE "athletes" ADD CONSTRAINT "fk_athletes_country_id" FOREIGN KEY("country_id")
+REFERENCES "countries" ("country_id");
 
-ALTER TABLE "coaches" ADD CONSTRAINT "fk_coaches_discipline" FOREIGN KEY("discipline")
-REFERENCES "teams" ("discipline");
+ALTER TABLE "athletes" ADD CONSTRAINT "fk_athletes_discipline_id" FOREIGN KEY("discipline_id")
+REFERENCES "disciplines" ("discipline_id");
 
-ALTER TABLE "medals" ADD CONSTRAINT "fk_medals_noc" FOREIGN KEY("noc")
-REFERENCES "athlete" ("noc");
+ALTER TABLE "coaches" ADD CONSTRAINT "fk_coaches_country_id" FOREIGN KEY("country_id")
+REFERENCES "medals" ("country_id");
+
+ALTER TABLE "coaches" ADD CONSTRAINT "fk_coaches_event_id" FOREIGN KEY("event_id")
+REFERENCES "events" ("event_id");
+
+ALTER TABLE "teams" ADD CONSTRAINT "fk_teams_discipline_id" FOREIGN KEY("discipline_id")
+REFERENCES "disciplines" ("discipline_id");
+
+ALTER TABLE "teams" ADD CONSTRAINT "fk_teams_event_id" FOREIGN KEY("event_id")
+REFERENCES "events" ("event_id");
 
